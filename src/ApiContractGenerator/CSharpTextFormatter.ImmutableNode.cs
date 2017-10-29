7b874394285e95f19fad390b60a58191b1fffc92
jnm2 <me@jnm2.com>
2017-10-29 15:30:16 -0400

    Moved CSharpTextFormatter nested types to their own files

---
+++ src/ApiContractGenerator/CSharpTextFormatter.ImmutableNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiContractGenerator
+{
+    public sealed partial class CSharpTextFormatter
+    {
+        private sealed class ImmutableNode<T>
+        {
+            public ImmutableNode(ImmutableNode<T> prev, T value, ImmutableNode<T> next)
+            {
+                Value = value;
+                Prev = prev;
+                Next = next;
+            }
+
+            public ImmutableNode<T> Prev { get; }
+            public T Value { get; }
+            public ImmutableNode<T> Next { get; }
+        }
+    }
+}

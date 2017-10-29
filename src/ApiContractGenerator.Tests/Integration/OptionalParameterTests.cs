02ad720b31cd67b19da7be7b9213904bfa79729b
jnm2 <me@jnm2.com>
2017-10-28 20:31:34 -0400

    Added failing tests on nullable and generic default values

--- src/ApiContractGenerator.Tests/Integration/OptionalParameterTests.cs
+++ src/ApiContractGenerator.Tests/Integration/OptionalParameterTests.cs
@@ -5,24 +5,55 @@ namespace ApiContractGenerator.Tests.Integration
     public sealed class OptionalParameterTests : IntegrationTests
     {
         [TestCase("bool x = false")]
+        [TestCase("bool? x = false")]
+        [TestCase("bool? x = null")]
         [TestCase(@"char x = '\0'")]
+        [TestCase(@"char? x = '\0'")]
+        [TestCase(@"char? x = null")]
         [TestCase("byte x = 0")]
+        [TestCase("byte? x = 0")]
+        [TestCase("byte? x = null")]
         [TestCase("sbyte x = 0")]
+        [TestCase("sbyte? x = 0")]
+        [TestCase("sbyte? x = null")]
         [TestCase("short x = 0")]
+        [TestCase("sbyte? x = 0")]
+        [TestCase("sbyte? x = null")]
         [TestCase("ushort x = 0")]
+        [TestCase("ushort? x = 0")]
+        [TestCase("ushort? x = null")]
         [TestCase("int x = 0")]
+        [TestCase("int? x = 0")]
+        [TestCase("int? x = null")]
         [TestCase("uint x = 0")]
+        [TestCase("uint? x = 0")]
+        [TestCase("uint? x = null")]
         [TestCase("long x = 0")]
+        [TestCase("long? x = 0")]
+        [TestCase("long? x = null")]
         [TestCase("ulong x = 0")]
+        [TestCase("ulong? x = 0")]
+        [TestCase("ulong? x = null")]
         [TestCase("float x = 0")]
+        [TestCase("float? x = 0")]
+        [TestCase("float? x = null")]
         [TestCase("double x = 0")]
+        [TestCase("double? x = 0")]
+        [TestCase("double? x = null")]
         [TestCase("object x = null")]
         [TestCase("string x = null")]
         [TestCase("System.IntPtr x = default")]
+        [TestCase("System.IntPtr? x = null")]
         [TestCase("System.UIntPtr x = default")]
+        [TestCase("System.UIntPtr? x = null")]
         [TestCase("System.DayOfWeek x = System.DayOfWeek.Sunday")]
+        [TestCase("System.DayOfWeek? x = System.DayOfWeek.Sunday")]
+        [TestCase("System.DayOfWeek? x = null")]
         [TestCase("System.AttributeTargets x = 0")]
+        [TestCase("System.AttributeTargets? x = 0")]
+        [TestCase("System.AttributeTargets? x = null")]
         [TestCase("System.DateTime x = default")]
+        [TestCase("System.DateTime? x = null")]
         [TestCase("System.Exception x = null")]
         [TestCase("System.Action x = null")]
         [TestCase("System.TypedReference x = default")]
@@ -32,6 +63,36 @@ namespace ApiContractGenerator.Tests.Integration
                 "public interface ITest",
                 "{",
                 "    void Method(" + parameterDefinition + ");",
+                "}"));
+        }
+
+        [Test]
+        public static void Default_value_is_generic_type_default()
+        {
+            Assert.That("public interface ITest { void Method<T>(T x = default); }", HasContract(
+                "public interface ITest",
+                "{",
+                "    void Method<T>(T x = default);",
+                "}"));
+        }
+
+        [Test]
+        public static void Default_value_is_generic_value_type_default()
+        {
+            Assert.That("public interface ITest { void Method<T>(T? x = null) where T : struct; }", HasContract(
+                "public interface ITest",
+                "{",
+                "    void Method<T>(T? x = null) where T : struct;",
+                "}"));
+        }
+
+        [Test]
+        public static void Default_value_is_generic_reference_type_default()
+        {
+            Assert.That("public interface ITest { void Method<T>(T x = null) where T : class; }", HasContract(
+                "public interface ITest",
+                "{",
+                "    void Method<T>(T x = null) where T : class;",
                 "}"));
         }
 

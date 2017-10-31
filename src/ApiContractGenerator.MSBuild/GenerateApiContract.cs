cae24e5adc50386db3df1dea78b3f7a1a8138471
jnm2 <me@jnm2.com>
2017-10-31 12:58:18 -0400

    Added MSBuild integration package

---
+++ src/ApiContractGenerator.MSBuild/GenerateApiContract.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using ApiContractGenerator.AssemblyReferenceResolvers;
+using ApiContractGenerator.MetadataReferenceResolvers;
+using ApiContractGenerator.Source;
+using Microsoft.Build.Framework;
+
+namespace ApiContractGenerator.MSBuild
+{
+    public sealed class GenerateApiContract : ITask
+    {
+        public IBuildEngine BuildEngine { get; set; }
+
+        public ITaskHost HostObject { get; set; }
+
+        [Required]
+        public ITaskItem[] Assemblies { get; set; }
+
+        public string[] IgnoredNamespaces { get; set; }
+
+        public bool Execute()
+        {
+            if (Assemblies.Length == 0) return true;
+
+            var generator = new ApiContractGenerator();
+            generator.IgnoredNamespaces.UnionWith(IgnoredNamespaces);
+
+            foreach (var assembly in Assemblies)
+            {
+                var assemblyPath = assembly.GetMetadata("ResolvedAssemblyPath");
+                var outputPath = assembly.GetMetadata("ResolvedOutputPath");
+
+                var assemblyResolver = new CompositeAssemblyReferenceResolver(
+                    new GacAssemblyReferenceResolver(),
+                    new SameDirectoryAssemblyReferenceResolver(Path.GetDirectoryName(assemblyPath)));
+
+                using (var metadataReferenceResolver = new MetadataReaderReferenceResolver(() => File.OpenRead(assemblyPath), assemblyResolver))
+                using (var source = new MetadataReaderSource(File.OpenRead(assemblyPath), metadataReferenceResolver))
+                using (var outputFile = File.CreateText(outputPath))
+                    generator.Generate(source, new CSharpTextFormatter(outputFile, metadataReferenceResolver));
+            }
+
+            return true;
+        }
+    }
+}

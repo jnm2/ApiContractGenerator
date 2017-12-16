e690d3a40807885716cb8ef4aee61f0ffb599499
jnm2 <me@jnm2.com>
2017-12-15 21:05:49 -0500

    More ignore tests

--- src/ApiContractGenerator.Tests/Integration/IgnoredNamespaceTests.cs
+++ src/ApiContractGenerator.Tests/Integration/IgnoredNamespaceTests.cs
@@ -15,5 +15,64 @@ namespace ApiContractGenerator.Tests.Integration
                 "    }",
                 "}").WithIgnoredNamespace("A"));
         }
+
+        [Test]
+        public static void Ignore_should_only_include_nested()
+        {
+            Assert.That("namespace A { public struct Test { } namespace B { public struct Test { } } }", HasContract(
+                "namespace A",
+                "{",
+                "    public struct Test",
+                "    {",
+                "    }",
+                "}").WithIgnoredNamespace("A.B"));
+        }
+
+        [Test]
+        public static void Ignore_should_not_match_nested()
+        {
+            Assert.That("namespace A.B { public struct Test { } }", HasContract(
+                "namespace A.B",
+                "{",
+                "    public struct Test",
+                "    {",
+                "    }",
+                "}").WithIgnoredNamespace("B"));
+        }
+
+        [Test]
+        public static void Ignore_should_include_nested()
+        {
+            Assert.That("namespace A { public struct Test { } namespace B { public struct Test { } } }", HasContract().WithIgnoredNamespace("A"));
+        }
+
+        [Test]
+        public static void Nonignored_method_should_unignore_return_type()
+        {
+            Assert.That("public struct Nonignored { public Ignored.ShouldUnignore Foo() => default; } namespace Ignored { public struct ShouldUnignore { } }", HasContract(
+                "public struct Nonignored",
+                "{",
+                "    public Ignored.ShouldUnignore Foo();",
+                "}",
+                "namespace Ignored",
+                "{",
+                "    public struct ShouldUnignore",
+                "    {",
+                "    }",
+                "}").WithIgnoredNamespace("Ignored"));
+        }
+
+        [TestCase("Ignored.ShouldUnignore p")]
+        [TestCase("ref Ignored.ShouldUnignore p")]
+        [TestCase("in Ignored.ShouldUnignore p")]
+        [TestCase("Ignored.ShouldUnignore p = default")]
+        public static void Nonignored_method_should_not_unignore_non_out_parameter_types(string parameterList)
+        {
+            Assert.That("public struct Nonignored { public void Foo(" + parameterList + ") { } } namespace Ignored { public struct ShouldUnignore { } }", HasContract(
+                "public struct Nonignored",
+                "{",
+                "    public void Foo(" + parameterList + ");",
+                "}").WithIgnoredNamespace("Ignored"));
+        }
     }
 }
